717c604ed4872da96feff30e48398e6969fc2595
kralljr <jenna.krall@gmail.com>
2021-03-14 20:59:47 -0400

    Add deerdat

---
+++ R/deerdat.R
@@ -0,0 +1,16 @@
+#' Hunter-killed deer in New Jersey
+#'
+#' Data on hunter-killed deer in New Jersey.
+#'
+#' \itemize{
+#' 		\item county. New Jersey county.  Hunterdon, Warren, and Hudson counties are not included.
+#' 		\item yr. Year of data.  Either 1997 or 2007.
+#' 		\item hk. Number of hunter-killed deer per square mile.
+#'}
+#'
+#' @docType data
+#' @keywords datasets
+#' @format A dataframe with 36 rows and 3 variables
+#' @name deerdat
+NULL
+
