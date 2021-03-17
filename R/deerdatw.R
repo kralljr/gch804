c14ea57a739b42729bf4eb21bc1ec20ee238dcd7
kralljr <jenna.krall@gmail.com>
2021-03-17 16:13:30 -0400

    Fix name of dataset

---
+++ R/deerdatw.R
@@ -0,0 +1,17 @@
+#' Hunter-killed deer in New Jersey
+#'
+#' Data on hunter-killed deer in New Jersey.
+#'
+#' \itemize{
+#' 		\item county. New Jersey county.  Hunterdon, Warren, and Hudson counties are not included.
+#' 		\item y1997. Number of hunter-killed deer per square mile in 1997.
+#' 		\item y2007 Number of hunter-killed deer per square mile in 2007
+
+#'}
+#'
+#' @docType data
+#' @keywords datasets
+#' @format A dataframe with 18 rows and 3 variables
+#' @name deerdatw
+NULL
+
